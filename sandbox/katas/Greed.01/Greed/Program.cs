1af1432363688759bbd9548e95111d4df05c492b
NataljaSavcuk <nataliya.s@centrum.cz>
2024-09-28 22:37:09 +0200

    Fit(Greed): Add Greed solution

---
+++ sandbox/katas/Greed.01/Greed/Program.cs
@@ -0,0 +1,3 @@
+﻿var calculation = new Greed.Calculation();
+Console.WriteLine("Hi there! Wellcome to our game! Have you already rolled the dice?");
+Console.WriteLine("Your great result is: " + calculation.FinalResults());
